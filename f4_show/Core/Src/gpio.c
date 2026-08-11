b2d218acb1b7bd1fbe7746f7a293072277fc8995
2h34 <671355617@qq.com>
2026-08-11 18:55:34 +0800

    第二次提交

--- f4_show/Core/Src/gpio.c
+++ f4_show/Core/Src/gpio.c
@@ -61,9 +61,13 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : INPUT_1_Pin */
   GPIO_InitStruct.Pin = INPUT_1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(INPUT_1_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
